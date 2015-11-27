b3c55b0d552a47c493017dd467bfa4ddc68695f3
Ilya Kholinow <i.kholinow@pixonic.com>
2015-11-28 00:56:16 +0300

    work on installation dialog

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Params
-INST_TYPE=0 # 0 - local 1 - remote
+INST_TYPE="Local" 
 INST_HOST='localhost'
 INSTALL_ECLIM=0 # 0 no 1 yes
+CONFIG_FILES=("./.tmux.conf" "./.vimrc")
+LINST_FILES=("./zide_install.sh" "./install_eclim.sh")
 
 # Calculated values
 pinst="apt-get install"
@@ -21,6 +23,11 @@ do
 	break
 done
 
+if [ "$INST_TYPE" == "Remote" ]; then
+	echo -n "Enter host to install: "
+	read INST_HOST
+fi
+
 echo -n "Install eclim (y/N)"
 read ans
 case "$ans" in
@@ -33,11 +40,30 @@ case "$ans" in
 esac
 }
 
+# Installation functions
+
+function local_install(){
+echo "Installing zsh,vim,tmux and others"
+echo ./zide_install.sh
+
+if [ $INSTALL_ECLIM -eq 1 ]; then
+	echo "Installing Eclim (with Vxfb)"
+	echo ./install_eclim.sh
+fi
+for file in ${CONFIG_FILES[@]}; do
+	echo cp "$file" ~/
+done
+
+}
 
 # Main execution thread
 function main(){
 select_i_type
-echo "Install type: $INST_TYPE. Install eclim: $INSTALL_ECLIM"
+echo "Install type: $INST_TYPE (host: $INST_HOST). Install eclim: $INSTALL_ECLIM"
+if [ "$INST_TYPE" == "Local" ]; then
+	local_install
+fi
+
 }
 
 main
